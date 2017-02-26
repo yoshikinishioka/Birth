8a563acbf75c6822617573a2d6c27a4237329a14
西岡佳樹 <nishiokayoshiki@M-Yoshiki-Nishioka-no-MacBook-Pro.local>
2017-02-27 02:06:35 +0900

    Initial Commit

---
+++ BIRTH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BIRTH
+//
+//  Created by 西岡佳樹 on 2017/02/27.
+//  Copyright © 2017年 Yoshiki Nishioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
